d327a77a80bd7f7eafa4a271819f99d56b2f2684
Whuss88 <williamhussey95@gmail.com>
2024-10-17 13:13:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log(`hello`)
+
+// create an object that holds freelancer info
+// put keys into object
+// put values for keys
+// make function for average price 
+// figure out how to make the list visible on the website
+// make list update every few seconds with new info(see notes from 10-17)
